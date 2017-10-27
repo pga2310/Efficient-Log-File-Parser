1b8fb69218d0036bd1c112183fd53c10e1f5efd9
Pushpak Gandhi <gandhipushpak@gmail.com>
2017-10-27 00:36:12 -0400

    Database Schema

---
+++ DB.sql
@@ -0,0 +1,46 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.6.17 : Database - parser
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`parser` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `parser`;
+
+/*Table structure for table `blocked_ip` */
+
+DROP TABLE IF EXISTS `blocked_ip`;
+
+CREATE TABLE `blocked_ip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(255) NOT NULL,
+  `comments` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip` (`ip`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `log_data` */
+
+DROP TABLE IF EXISTS `log_data`;
+
+CREATE TABLE `log_data` (
+  `date` datetime NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `request` text NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `user_agent` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
